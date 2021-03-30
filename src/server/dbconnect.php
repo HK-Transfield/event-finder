fe842a5fbd742a8be705f46aee4b87cf8a4e1092
ZaraDev-Tempest <74291872+ZaraDev-Tempest@users.noreply.github.com>
2021-03-30 15:28:53 +1300

    Delete src directory

--- src/server/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try {
-        $con = new PDO('mysql:host=localhost; dbname=zaradev', 'root', '');
-
-    } catch(PDOException $e) {
-        echo "Database connection error ". $e->getMessage();
-    }
